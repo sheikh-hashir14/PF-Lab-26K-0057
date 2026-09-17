40d6a60efd967ba1c436692a4065354839e410fc
sheikh-hashir14 <shk.muhammadhashir14@gmail.com>
2026-09-17 13:11:50 +0500

    move files + q6

---
+++ lab-rev/q7-26K-0057.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter a Positive Integer: ");
+    scanf("%d", &n);
+
+    switch(n){
+        case 1: 
+            printf("one\n");
+            break;
+        case 2:
+            printf("two\n");
+            break;
+        case 3: 
+            printf("three\n");
+            break;
+        case 4: 
+            printf("four\n");
+            break;
+        case 5: 
+            printf("five\n");
+            break;
+        case 6: 
+            printf("six\n");
+            break;
+        case 7: 
+            printf("seven\n");
+            break;
+        case 8: 
+            printf("eight\n");
+            break;
+        case 9: 
+            printf("nine\n");
+            break;
+        default:
+            printf("n is greater than 9\n");
+            break;
+    }
+}
